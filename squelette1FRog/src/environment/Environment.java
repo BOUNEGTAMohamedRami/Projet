37bdf8b4193a15a4d283c47dcfb05ed7d888be4f
BOUNEGTAMohamedRami <ramisteryio@hotmail.frr>
2020-11-30 18:51:40 +0100

    Car move

--- squelette1FRog/src/environment/Environment.java
+++ squelette1FRog/src/environment/Environment.java
@@ -14,16 +14,24 @@ public class Environment implements IEnvironment {
     private Game game ;
 
     public Environment(Game game) {
+        this.game = game;
+        this.roadLines = new ArrayList<>();
+        this.roadLines.add(new Lane(game, 0));
+        for (int i =1 ; i < game.height; i++) {
 
-
+            this.roadLines.add(new Lane(game, i));
+        }
+        this.roadLines.add(new Lane( game , game.height - 1));
     }
     @Override
     public boolean isSafe(Case c) {
+
      return false;
     }
         @Override
         public boolean isWinningPosition (Case c){
-    return false;
+        return c == new Case(c.absc,game.height-1);
+
         }
 
         @Override
